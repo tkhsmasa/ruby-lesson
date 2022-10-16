a76d70ebfceda8ff25cb2c1ab8bb89778d7a8019
tkhsmasa <115441873+tkhsmasa@users.noreply.github.com>
2022-10-16 04:58:48 +0000

    [start]lesson6-1

---
+++ example/apple.rb
@@ -0,0 +1,9 @@
+apple = "Yamanashi"
+
+if apple =="Aomori"
+  puts "このリンゴは青森さんです"
+elsif apple == "Nagano"
+  puts "このリンゴは青森さんではなく、長野産です"
+else
+ puts "このリンゴは青森産でも長野産でもありません"
+end
---
+++ lesson5.rb
@@ -0,0 +1,9 @@
+total_preice = 10
+
+if total_preice > 100
+  puts "みかんを購入。所持金に余りあり。"
+elsif total_preice == 100
+  puts "みかんを購入。所持金は0円。"
+elsif total_preice < 100
+  puts "みかんを購入することができません。"
+end
